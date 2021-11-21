12947479482fef9ee7e6f405ab491b435812b5d9
iamAravindks <aravindkadaikattu@gmail.com>
2021-11-21 22:57:03 +0530

    Add the head section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+
 
 
 function App() {
   return (
-<NavBar/>
+    <>
+      <NavBar />
+      <Header />
+      </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
-
-
+import ieee from "../assets/ieee.svg";
+import iedc from "../assets/IEDC.svg";
+import pie from "../assets/PIE.svg";
+import sb from "../assets/SB.svg";
+import tbi from "../assets/TBI.svg";
+import upscale from '../assets/upscale.svg'
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+      <div className='container-fluid '>
+          <div className="logo-container">
+              <img className="logo" src={upscale} alt="Upscale'21"/>
+          </div>
+      <div className='img-tags'>
+        <img className='sponsor-img' src={ieee} alt='IEEE' />
+        <img className='sponsor-img' src={iedc} alt='IEDC' />
+        <img className='sponsor-img' src={pie} alt='PIE' />
+        <img className='sponsor-img' src={sb} alt='IEEE SB Palakkad' />
+        <img className='sponsor-img' src={tbi} alt='TBI GEC Palakkad' />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
