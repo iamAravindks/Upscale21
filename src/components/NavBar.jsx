9f6ca20466bd9d892a8ccb6785654a9f91efd4cc
iamAravindks <aravindkadaikattu@gmail.com>
2021-11-21 20:34:04 +0530

    Fix the typo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,10 @@
 
 const NavBar = () => {
     return (
-      <nav class='navbar navbar-expand-lg navbar navbar-dark bg-dark '>
-        <div class='container-fluid'>
+      <nav className='navbar navbar-expand-lg navbar navbar-dark bg-dark '>
+        <div className='container-fluid'>
           <button
-            class='navbar-toggler'
+            className='navbar-toggler'
             type='button'
             data-bs-toggle='collapse'
             data-bs-target='#navbarNav'
@@ -13,34 +13,34 @@ const NavBar = () => {
             aria-expanded='false'
             aria-label='Toggle navigation'
           >
-            <span class='navbar-toggler-icon'></span>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div class='collapse navbar-collapse' id='navbarNav'>
-            <ul class='navbar-nav'>
-              <li class='nav-item'>
-                <a class='nav-link active' aria-current='page' href='/'>
+          <div className='collapse navbar-collapse' id='navbarNav'>
+            <ul className='navbar-nav'>
+              <li className='nav-item'>
+                <a className='nav-link active' aria-current='page' href='/'>
                   Home
                 </a>
               </li>
-              <li class='nav-item'>
-                <a class='nav-link' href='#About'>
+              <li className='nav-item'>
+                <a className='nav-link' href='#About'>
                   About
                 </a>
               </li>
-              <li class='nav-item'>
-                <a class='nav-link' href='#Schedule'>
+              <li className='nav-item'>
+                <a className='nav-link' href='#Schedule'>
                   Schedule
                 </a>
               </li>
-              <li class='nav-item'>
-                <a class='nav-link' href='#Speakers'>
+              <li className='nav-item'>
+                <a className='nav-link' href='#Speakers'>
                   Speakers
                 </a>
               </li>
-              <li class='nav-item'>
+              <li className='nav-item'>
                 <button
                   type='button'
-                  class='btn btn-light '
+                  className='btn btn-light '
                   style={{ margin: "0 15px" }}
                 >
                   Register
