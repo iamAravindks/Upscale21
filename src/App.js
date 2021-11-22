eba623772d6dbbfa1089aed551ce0b1410c3a91a
athul <athuljoshi7@gmail.com>
2021-11-22 21:20:20 +0530

    footer completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Info from "./components/Info";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <Header />
       <Info/>
+      <Footer />
       </>
   );
 }
