459d25b71887b9192e65b473ef443fb47027d581
iamAravindks <aravindkadaikattu@gmail.com>
2021-11-23 17:39:42 +0530

    fix the speaker section-3

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -19,7 +19,8 @@ const Speakers = () => {
             />
           ))}
         </div>
-      </div>
+        </div>
+        </div>
     );
 }
 
--- src/components/speaker/SpeakerCard.jsx
+++ src/components/speaker/SpeakerCard.jsx
@@ -23,27 +23,9 @@ const SpeakerCard = ({ name, title, topic, imgSrc }) => {
       <div className="speaker-card-topic">
         <p className='card-text'>{topic}</p>
       </div>
+      </div>
       
 
-
-const SpeakerCard = ({ name, title, topic, imgSrc }) => {
-  return (
-    <div className='card speaker-card'>
-      <div className="speaker-head">
-        <img
-          src={imgSrc}
-          className='card-img-top rounded-circle speaker-img'
-          alt='...'
-        />
-      </div>
-      <div className='card-body'>
-        <h5 className='card-title'>{name}</h5>
-        <p className='card-text'>{title}</p>
-        <p className='card-text'>{topic}</p>
-      </div>
-
-    </div>
-  );
-};
+)}
 
 export default SpeakerCard;
