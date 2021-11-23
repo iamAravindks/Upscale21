70c58d105dd9f74f5d042f158a362499d35101a5
Jaison Joshi <54494294+jaisonjoshi@users.noreply.github.com>
2021-11-23 15:03:26 +0530

    Add files via upload

---
+++ src/components/speaker/SpeakerCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import bgimg from '../../assets/ellpsebg.png';
+
+const SpeakerCard = ({ name, title, topic, imgSrc }) => {
+  return (
+    <div className='speaker-card'>
+      <div className="speaker-head">
+        <img src={bgimg} className="ellpsebg" alt="ha" />
+        <div className="speaker-img-contaner">
+          <img 
+            src={imgSrc}
+            className='card-img-top rounded-circle speaker-img'
+            alt='...'
+          />
+        </div>
+      </div>
+      <div className='card-body'>
+        <em><h5 className='card-title'>{name}</h5></em>
+        <p className='card-sub-title'>{title}</p>
+        
+      </div>
+      <div className="speaker-card-topic">
+        <p className='card-text'>{topic}</p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default SpeakerCard;
